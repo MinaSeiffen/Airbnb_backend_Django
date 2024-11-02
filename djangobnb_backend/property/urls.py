c110432d9d64275506961ac10734f4976609dc0e
MinaSeiffen <minaseiffen@gmail.com>
2024-11-02 12:08:53 +0200

    Booking is almost done

--- djangobnb_backend/property/urls.py
+++ djangobnb_backend/property/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", api.properties_list, name="properties_list"),
     path("create/", api.create_property, name="create_properties"),
     path("<uuid:pk>/", api.property_detail, name="properties_detail"),
+    path("<uuid:pk>/book/", api.book_property, name="book_property"),
 ]
